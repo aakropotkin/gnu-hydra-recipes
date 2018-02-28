53f4989bd8f5f4c3007ee64ea388b0f2a8ba1349
Glenn Morris <rgm@gnu.org>
2018-02-28 15:06:32 -0500

    emacs: disable more optional features in tarball build

--- emacs/release.nix
+++ emacs/release.nix
@@ -71,7 +71,7 @@ in
 
       tarball = pkgs: {
 	# FIXME Move --enable-check-lisp-object-type here from coverage?
-	configureFlags = "--with-gnutls=no";
+	configureFlags = "--with-gnutls=no --with-file-notification=no --with-sound=no --without-threads";
 	buildInputs = with pkgs; [ texinfo ncurses pkgconfig perl git ];
 
         # patches = [ ./bug11251.patch ];
