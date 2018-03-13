b08d55267c373b31d5be622783fc1e3249b47169
Glenn Morris <rgm@gnu.org>
2018-03-13 19:28:11 -0400

    emacs: run make check using .elc files, not .el files

--- emacs/release.nix
+++ emacs/release.nix
@@ -130,7 +130,7 @@ in
 	doCheck = true;
 	configureFlags = "--enable-profiling --enable-check-lisp-object-type --with-modules" ;
 	checkPhase = ''
-          make check-expensive EMACS_HYDRA_CI=1 TEST_BACKTRACE_LINE_LENGTH=150
+          make check-expensive EMACS_HYDRA_CI=1 TEST_BACKTRACE_LINE_LENGTH=150 TEST_LOAD_EL=no
           mkdir -p "$out/nix-support"
           find test -name '*.log' > test.tmp
           if test -s test.tmp; then
