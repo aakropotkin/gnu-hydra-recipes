95381c3da0721429e474eb435fcc654db4925313
Glenn Morris <rgm@gnu.org>
2018-12-03 17:27:34 -0500

    emacs: try xcodebuild instead of xcbuild

--- emacs/release.nix
+++ emacs/release.nix
@@ -55,7 +55,7 @@ let
          [ gtkLibs.gtk librsvg acl dbus gnutls gpm libselinux imagemagick
            libpng libjpeg libungif libtiff libxml2 ])
 
-    ++ stdenv.lib.optional stdenv.isDarwin xcbuild
+    ++ stdenv.lib.optional stdenv.isDarwin xcodebuild
 
     # Fallback for non-GNU systems.
     ++ (stdenv.lib.optional (!stdenv.isLinux) xlibs.libXaw);
