e56e549c2fb66d199d86dce7130a957ad4e37a90
Glenn Morris <rgm@gnu.org>
2018-12-10 19:07:11 -0500

    emacs: disable check-expensive in coverage due to tramp timeout

--- emacs/release.nix
+++ emacs/release.nix
@@ -129,7 +129,7 @@ in
 	doCheck = true;
 	configureFlags = "--enable-profiling --enable-check-lisp-object-type --with-modules" ;
 	checkPhase = ''
-          make check-expensive EMACS_HYDRA_CI=1 TEST_BACKTRACE_LINE_LENGTH=150 TEST_LOAD_EL=no SUMMARIZE_TESTS=25
+          make check EMACS_HYDRA_CI=1 TEST_BACKTRACE_LINE_LENGTH=150 TEST_LOAD_EL=no SUMMARIZE_TESTS=25
           mkdir -p "$out/nix-support"
           find test -name '*.log' > test.tmp
           if test -s test.tmp; then
